79969c0c457e7e46af8a7085c0b6d76e3dc2527d
Michael Steven Chan Gonzalez <cha18562@uvg.edu.gt>
2019-04-05 18:42:25 -0600

    Junit tests

---
+++ src/PatientTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PatientTest {
+
+    @Test
+    public void getPriority() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void getInfo() {
+    }
+}
---
+++ src/VectorHeapTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+public class VectorHeapTest {
+
+    @org.junit.Test
+    public void parent() {
+    }
+
+    @org.junit.Test
+    public void left() {
+    }
+
+    @org.junit.Test
+    public void right() {
+    }
+
+    @org.junit.Test
+    public void percolateUp() {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void pushDownRoot() {
+    }
+
+    @org.junit.Test
+    public void remove() {
+    }
+
+    @org.junit.Test
+    public void getFirst() {
+    }
+
+    @org.junit.Test
+    public void isEmpty() {
+    }
+
+    @org.junit.Test
+    public void size() {
+    }
+
+    @org.junit.Test
+    public void clear() {
+    }
+}
